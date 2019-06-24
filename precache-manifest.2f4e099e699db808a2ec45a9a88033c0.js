e95aad3c1093ab1c028e1de953ebe418523ee086
kodi24fever <frankdavid93om@yahoo.com>
2019-06-24 00:06:13 -0400

    Updates

---
+++ precache-manifest.2f4e099e699db808a2ec45a9a88033c0.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d129c05ced002797e0ac3596c7046165",
+    "url": "/fdaviz/index.html"
+  },
+  {
+    "revision": "94fbe5872aa08c8507f0",
+    "url": "/fdaviz/static/css/2.7f1794a0.chunk.css"
+  },
+  {
+    "revision": "9569acdc0daf2cd28df5",
+    "url": "/fdaviz/static/css/main.86db518e.chunk.css"
+  },
+  {
+    "revision": "94fbe5872aa08c8507f0",
+    "url": "/fdaviz/static/js/2.f3c425b8.chunk.js"
+  },
+  {
+    "revision": "9569acdc0daf2cd28df5",
+    "url": "/fdaviz/static/js/main.e488340e.chunk.js"
+  },
+  {
+    "revision": "949a9421eed578d7280c",
+    "url": "/fdaviz/static/js/runtime~main.2e0877cc.js"
+  },
+  {
+    "revision": "43f62b103c33f53ea8becff3f1725ac6",
+    "url": "/fdaviz/static/media/Vmarine.43f62b10.jpg"
+  },
+  {
+    "revision": "d7b4a5cb993a4a88ce3f96ecf2477b73",
+    "url": "/fdaviz/static/media/aguaDeLuz-1.d7b4a5cb.png"
+  },
+  {
+    "revision": "427cf2c0c4093d111e3270f375040b0b",
+    "url": "/fdaviz/static/media/aguaDeLuz-2.427cf2c0.png"
+  },
+  {
+    "revision": "d3a2220fa7c91b332ba1878195aff059",
+    "url": "/fdaviz/static/media/aguaDeLuz-3.d3a2220f.png"
+  },
+  {
+    "revision": "aaccde7267457b089b51c7e2096e39dd",
+    "url": "/fdaviz/static/media/aguaDeLuz-4.aaccde72.png"
+  },
+  {
+    "revision": "851ac5cd41c1272715245e19eb765cce",
+    "url": "/fdaviz/static/media/aguaDeLuz.851ac5cd.png"
+  },
+  {
+    "revision": "dee3edefffb53259fae83bfc83f5cc0c",
+    "url": "/fdaviz/static/media/amtbw.dee3edef.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/fdaviz/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fdaviz/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fdaviz/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fdaviz/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fdaviz/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fdaviz/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/fdaviz/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/fdaviz/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/fdaviz/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/fdaviz/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "207ed67d4f2015843b9b34015fada443",
+    "url": "/fdaviz/static/media/medlingos-1.207ed67d.jpg"
+  },
+  {
+    "revision": "b9e1984d43c046beaeff79ecd464f440",
+    "url": "/fdaviz/static/media/medlingos-2.b9e1984d.jpg"
+  },
+  {
+    "revision": "490444ab7e3c808620276a7af65ea027",
+    "url": "/fdaviz/static/media/medlingos-3.490444ab.jpg"
+  },
+  {
+    "revision": "84190118ecc94671eebf9a45df58c72f",
+    "url": "/fdaviz/static/media/medlingos-4.84190118.jpg"
+  },
+  {
+    "revision": "241b7ff1c8e16a0cb79a3413ea99a65d",
+    "url": "/fdaviz/static/media/medlingos.241b7ff1.jpg"
+  },
+  {
+    "revision": "d593b2eadd6ee5da52fea15876ae5c53",
+    "url": "/fdaviz/static/media/product.d593b2ea.png"
+  },
+  {
+    "revision": "66ee3e33ad39b3bea037223e540db558",
+    "url": "/fdaviz/static/media/todo-1.66ee3e33.png"
+  },
+  {
+    "revision": "048ab149bd392c063c187556b2583b24",
+    "url": "/fdaviz/static/media/todo-2.048ab149.png"
+  },
+  {
+    "revision": "7282f3f47663696d172857d2a8467b49",
+    "url": "/fdaviz/static/media/todo-3.7282f3f4.png"
+  },
+  {
+    "revision": "ac82d97d170d742395675707f640e98d",
+    "url": "/fdaviz/static/media/todolist.ac82d97d.png"
+  },
+  {
+    "revision": "4cf363f8f9a0fad82f3397e3b88e7a75",
+    "url": "/fdaviz/static/media/vmarine-1.4cf363f8.png"
+  },
+  {
+    "revision": "ce05e1d947986ea5db8cdc20a888d09f",
+    "url": "/fdaviz/static/media/vmarine-2.ce05e1d9.png"
+  },
+  {
+    "revision": "df3dafe0c1e793e5697303556bf88884",
+    "url": "/fdaviz/static/media/vmarine-3.df3dafe0.png"
+  },
+  {
+    "revision": "8db57d5a31d63446801b3885a57c79f3",
+    "url": "/fdaviz/static/media/vmarine-5.8db57d5a.png"
+  },
+  {
+    "revision": "6e2642f8f10d6c00c86c28de0be68725",
+    "url": "/fdaviz/static/media/vmarine-6.6e2642f8.png"
+  },
+  {
+    "revision": "a16ea18c42061c568f85f0677570d157",
+    "url": "/fdaviz/static/media/vmarine-7.a16ea18c.png"
+  }
+]);
